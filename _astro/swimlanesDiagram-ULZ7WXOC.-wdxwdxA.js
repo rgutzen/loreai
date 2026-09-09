8815a85f029f981f6a222982afe7548dc0ac1240
rgutzen <robin.gutzen@outlook.com>
2026-09-09 10:03:12 +0000

    Deploying to gh-pages from @ rgutzen/loreai@8475ae4237b242b801f9bc1be5f8cccd78a629db 🚀

---
+++ _astro/swimlanesDiagram-ULZ7WXOC.-wdxwdxA.js
@@ -0,0 +1,8 @@
+import"./src.OgxTg4hg.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-I66GZJ75.Wgqabmh3.js";import"./chunk-NSK5VX7P.CsINghoR.js";import"./chunk-ZIRB5QZD.Dh20_f-h.js";import"./chunk-W5SLKNZC.DhpAzw7u.js";import"./chunk-UBXNYLIW.DXvavivP.js";import"./chunk-4I5QYGJK.BrRMZ5RX.js";import"./chunk-WRU74C26.CenapLoI.js";import"./chunk-QR6OTTB3.Bfh_PM-9.js";import"./chunk-7BUUIJ7U.CAhmNhj7.js";import"./chunk-7Z6QIM7H.29mL7bn_.js";import"./chunk-J7OUQ5F2.BkWtVEEb.js";import"./chunk-2GRJ4B5K.CG2da4qL.js";import"./chunk-XXDRQBXY.B0iYp7hA.js";import"./chunk-KBJHAD2P.QIACLorB.js";import{r as t,t as n}from"./chunk-JQJVKLGR.DetA4qQS.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
